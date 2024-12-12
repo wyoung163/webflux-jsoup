8da90340377ac4fe98eb0b63b28dcc4c348b67a0
wyoung163 <20201014@sungshin.ac.kr>
2024-12-12 20:47:24 +0900

    init: initial setting

---
+++ src/main/java/com/example/webflux/WebfluxApplication.java
@@ -0,0 +1,19 @@
+package com.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@EnableScheduling // 특정 시간 마다 동작 반복
+@Configuration
+@SpringBootApplication
+public class WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApplication.class, args);
+	}
+
+}
