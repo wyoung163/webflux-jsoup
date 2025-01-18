14d90a6175493890f44f07044af5dcfbc6e11d13
wyoung163 <20201014@sungshin.ac.kr>
2025-01-18 21:32:40 +0900

    refactor: extract information using iterator

--- src/main/java/com/example/webflux/Service/CareerService.java
+++ src/main/java/com/example/webflux/Service/CareerService.java
@@ -13,6 +13,7 @@ import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 @Service
 @RequiredArgsConstructor
@@ -35,12 +36,50 @@ public class CareerService {
             String link = e.select("a.card_link").attr("onclick").toString(); //.substring(6, -3);
             String title = e.select("h4.card_title").text();
             String company = title.split("]")[0].replace("[", "");
-            String[] info = e.select("dd.info_text").text().split(" ");
-            String category = info[0];
-            String team = info[1];
-            String experience = info[2];
-            String condition = info[3];
-            String duration = info[4];
+            Iterator<Element> iterator = e.select("dd.info_text").stream().iterator();
+            String category = iterator.next().text();
+            String team = iterator.next().text();
+            String experience = iterator.next().text();
+            String condition = iterator.next().text();
+            String duration = iterator.next().text();
+
+            careers.add(
+                    Career.builder()
+                            .link(link)
+                            .title(title)
+                            .company(company)
+                            .category(category)
+                            .team(team)
+                            .experience(experience)
+                            .conditions(condition)
+                            .duration(duration)
+                            .build()
+            );
+        }
+
+        return careerRepository.saveAll(
+                careers
+        );
+    }
+
+    public Flux<Career> getNaverCloudCareers() throws IOException {
+        connection = Jsoup.connect("https://recruit.navercloudcorp.com/rcrt/list.do");
+        document = connection.get();
+        elements = document.select("ul[class=card_list]").select("li.card_item").select("a.card_link");
+
+        ArrayList<Career> careers = new ArrayList<>();
+
+        int idx = 0;
+        for(Element e: elements) {
+            String link = e.select("a.card_link").attr("onclick").toString();//.substring(6, -3);
+            String title = e.select("h4.card_title").text();
+            String company = title.split("]")[0].replace("[", "");
+            Iterator<Element> iterator = e.select("dd.info_text").stream().iterator();
+            String category = iterator.next().text();
+            String team = iterator.next().text();
+            String experience = iterator.next().text();
+            String condition = iterator.next().text();
+            String duration = iterator.next().text();
 
             careers.add(
                     Career.builder()
