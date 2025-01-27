3c3d2bf7932fbc7bf99c7ba3997de471708e5bd5
wyoung163 <20201014@sungshin.ac.kr>
2025-01-27 21:18:58 +0900

    fix: add select querys to use in mail service

--- src/main/java/com/example/webflux/Repository/UserRepository.java
+++ src/main/java/com/example/webflux/Repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.webflux.Repository;
 
 import com.example.webflux.Domain.User;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+    @Query("SELECT email FROM user")
+    public Flux<String> findAllEmails();
 }
