4cc1c6f624b28dbb699d94bea565293e6ccc06d3
wyoung163 <20201014@sungshin.ac.kr>
2025-01-27 21:21:58 +0900

    fix: delete scheduling

--- src/main/java/com/example/webflux/Controller/CareerController.java
+++ src/main/java/com/example/webflux/Controller/CareerController.java
@@ -3,6 +3,7 @@ package com.example.webflux.Controller;
 import com.example.webflux.Domain.Career;
 import com.example.webflux.Service.CareerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +14,22 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 public class CareerController {
+    @Autowired
     private final CareerService careerService;
 
-    @Scheduled(cron = "0 0 0/6 * * *")
+    //@Scheduled(cron = "0 0 0/6 * * *")
     @GetMapping("/career/naver")
     public Flux<Career> searchNaverCareers() throws IOException {
         return careerService.getNaverCareers();
     }
 
-    @Scheduled(cron = "0 0 0/6 * * *")
+    //@Scheduled(cron = "0 0 0/6 * * *")
     @GetMapping("/career/navercloud")
     public Flux<Career> searchNaverCloudCareers() throws IOException {
         return careerService.getNaverCloudCareers();
     }
 
-    @Scheduled(cron = "0 0 0/6 * * *")
+    //@Scheduled(cron = "0 0 0/6 * * *")
     @GetMapping("/career/line")
     public Flux<Career> searchLineCareers() throws IOException{
         return careerService.getLineCareers();
